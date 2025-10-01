56447768cf11992a02e3b21f0dd766a30f79a5ce
Jimmy <jfs999912@gmail.com>
2025-10-01 14:25:43 -0600

    commit desde TERMUX

---
+++ if.s
@@ -0,0 +1,17 @@
+.global main
+;.equ PORTB, 0x05                   ;.equ DDRB, 0x04
+; .equ PORTD, 0x0B                  ; .equ DDRD, 0x0A                   ; .equ PIND, 0x09
+;.equ SPH, 0x3E
+;equ SPL, 0x3D
+; este es le if  then  else en ensamblador
+; va variando entre else y then al cambiar el valor de 25 a 26 segun condicion
+main:
+	LDI R16, 25  ; registro R16 se asigna el valor 25
+	LDI R17, 25  ; registro R16 se asigna el valor 25	
+loop:
+	CP R16, R17  ; comnpara registros R16 y R17 afecta banderas Z, N, V, C, H
+	BREQ else  ; si son iguales va a "else" Si no va a "then"					
+then:	LDI R17, 25
+	JMP done  ; salta a done para repetirse
+else:	LDI R17, 26		
+done:	JMP loop  ; se repite
